3f6d5363eb2bb54799a2b339b991e545e04e4cd3
constanzamorales <constanzabmorales@gmail.com>
2023-06-23 15:10:42 -0400

    Testing OAuth

--- server/index.js
+++ server/index.js
@@ -18,5 +18,12 @@ passport.use(
 	)
 );
 
+app.get(
+	"/auth/google",
+	passport.authenticate("google", {
+		scope: ["profile", "email"]
+	})
+);
+
 const PORT = process.env.PORT || 5050;
 app.listen(PORT);
