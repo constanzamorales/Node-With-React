1825bce6e523d3fc52d9c14054e03689ee6deba3
constanzamorales <constanzabmorales@gmail.com>
2023-06-26 12:03:05 -0400

    Deleted the else

--- server/services/passport.js
+++ server/services/passport.js
@@ -29,12 +29,11 @@ passport.use(
 
 			if (existingUser) {
 				// We already have a record with the given profile ID
-				done(null, existingUser); // This tells passport that there's no error and there's the user we asked for
-			} else {
-				// Make a new record
-				const user = await new User({ googleId: profile.id }).save(); // It will save it in the database for us
-				done(null, user);
+				return done(null, existingUser); // This tells passport that there's no error and there's the user we asked for
 			}
+			// Make a new record
+			const user = await new User({ googleId: profile.id }).save(); // It will save it in the database for us
+			done(null, user);
 		}
 	)
 );
