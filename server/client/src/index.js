e0903e7f6c931dd3af952df2598797569c36b406
constanzamorales <constanzabmorales@gmail.com>
2023-06-26 12:51:05 -0400

    Finishing reducer setup

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,9 +1,11 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import App from "./components/App";
 
-const store = createStore(() => [], {}, applyMiddleware());
+import App from "./components/App";
+import reducers from "./reducers";
+
+const store = createStore(reducers, {}, applyMiddleware());
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
