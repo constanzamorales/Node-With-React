3c8f5062308c51dfd10f43ce4f146ec46a1caa64
constanzamorales <constanzabmorales@gmail.com>
2023-06-29 19:41:48 -0400

    Payment fixes

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -15,11 +15,14 @@ class Header extends Component {
 					</li>
 				);
 			default:
-				return (
-					<li>
-						<a>Logout</a>
+				return [
+					<li key="1">
+						<Payments />
+					</li>,
+					<li key="2">
+						<a href="/api/logout">Logout</a>
 					</li>
-				);
+				];
 		}
 	}
 
