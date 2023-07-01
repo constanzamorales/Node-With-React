3e4f69a797d9da1081e276cea834dd247bb05ab0
constanzamorales <constanzabmorales@gmail.com>
2023-07-01 11:40:24 -0400

    Requiring authentication

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -3,6 +3,10 @@ const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = (app) => {
 	app.post("/api/stripe", async (req, res) => {
+		if (!req.user) {
+			return res.status(401).send({ error: "You must log in." });
+		}
+
 		const charge = await stripe.charges.create({
 			amount: 500, // We have to say it again in the backend
 			currency: "usd",
