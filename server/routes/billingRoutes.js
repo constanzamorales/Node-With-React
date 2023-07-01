96625e365ba61b278847b947056fc9074f4ad344
constanzamorales <constanzabmorales@gmail.com>
2023-07-01 11:24:35 -0400

    Finalizing a charge

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -2,12 +2,14 @@ const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = (app) => {
-	app.post("/api/stripe", (req, res) => {
-		stripe.charges.create({
+	app.post("/api/stripe", async (req, res) => {
+		const charge = await stripe.charges.create({
 			amount: 500, // We have to say it again in the backend
 			currency: "usd",
 			description: "$5 for 5 credits",
 			source: req.body.id //the token stripe gave us for the credit card
 		});
+
+		console.log(charge);
 	});
 };
