5c6355dc605e32650eb5e6cb6e3bd7a074db0fad
constanzamorales <constanzabmorales@gmail.com>
2023-07-01 12:23:36 -0400

    Routing in production

--- server/index.js
+++ server/index.js
@@ -24,6 +24,17 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+if (process.env.NODE_ENV == "production") {
+	// Express will serve up production assets
+	app.use(express.static("client/build"));
+
+	// Express will serve up the index.html file if it doesn't recognize the route
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 // Instead of importing with a const, we immediately call that authRoutes() function
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
