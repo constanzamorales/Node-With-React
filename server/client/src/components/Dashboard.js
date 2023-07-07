dc4abbe315aa5153474efcac354e280d2c466352
constanzamorales <constanzabmorales@gmail.com>
2023-07-07 18:19:21 -0400

    Working on the dashboard, added FAB

---
+++ server/client/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+const Dashboard = () => {
+	return (
+		<div>
+			Dashboard
+			<div className="fixed-action-btn">
+				<a className="btn-floating btn-large red">
+					<i className="material-icons">add</i>
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default Dashboard;
