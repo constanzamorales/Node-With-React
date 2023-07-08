d827408fc0a6757184f7031e005e0322f651461a
constanzamorales <constanzabmorales@gmail.com>
2023-07-08 13:50:20 -0400

    Displaying invalid emails

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -3,6 +3,7 @@ import { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
+import validateEmails from "../../utils/validateEmails";
 
 const FIELDS = [
 	{
@@ -60,6 +61,8 @@ class SurveyForm extends Component {
 
 function validate(values) {
 	const errors = {};
+
+	errors.emails = validateEmails(values.emails || "");
 
 	_.each(FIELDS, ({ name }) => {
 		if (!values[name]) {
