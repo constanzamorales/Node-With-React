57d3d442585d52d2d7ad0148882caf5354061ca4
constanzamorales <constanzabmorales@gmail.com>
2023-07-08 11:08:51 -0400

    Navigating with the add button

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Dashboard = () => {
 	return (
 		<div>
 			Dashboard
 			<div className="fixed-action-btn">
-				<a className="btn-floating btn-large red">
+				<Link to="/surveys/new" className="btn-floating btn-large red">
 					<i className="material-icons">add</i>
-				</a>
+				</Link>
 			</div>
 		</div>
 	);
