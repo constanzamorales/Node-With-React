de9720784d601fc8c10290326a416e8c04b806fd
constanzamorales <constanzabmorales@gmail.com>
2023-07-09 12:17:52 -0400

    Setting up and testing webhooks

--- server/services/emailTemplates/surveyTemplate.js
+++ server/services/emailTemplates/surveyTemplate.js
@@ -10,8 +10,8 @@ module.exports = (survey) => {
 					<p>${survey.body}</p>
 				</div>
 				<div>
-					<a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
-					<a href="${keys.redirectDomain}/api/surveys/thanks">No</a>
+					<a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
+					<a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
 				</div>
 			</body>
 		</html>
